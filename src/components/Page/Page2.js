8f07c9fe63d5c3b9d54cc2436d154af7eaad12a0
Anastasiia Bakuta <ae@anadeainc.com>
2018-02-18 19:28:41 +0200

    Added Page 2

--- src/components/Page/Page2.js
+++ src/components/Page/Page2.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, Image } from 'react-native'
 import PropTypes from 'prop-types'
 
 import styles from './styles'
 import Hint from '../Hint'
 import Button from '../Button'
 import Input from '../Input'
+
+import fox3 from '../../images/fox/f3.png'
 
 export default class Page2 extends Component {
   static propTypes = {
@@ -18,27 +20,43 @@ export default class Page2 extends Component {
     return (
       <View style={styles.containerView}>
         <Text style={styles.title}>
-          Page 2
+          Вот как новость... 
         </Text>
         <Text style={styles.paragraph}>
-          Press Cmd+R to reload,\n Cmd+D or shake for dev menu
+          Даже не знаю, как ты будешь теперь сидеть на этом стуле. Я бы на твоём месте задумался насчет этих двоих...
+        </Text>
+        <Text style={styles.paragraph}>
+          Ну ладно. Как тебе мой стихотворный слог?)) Почти Пушкин? Считаешь? Только аккуратнее выбирай мне похвалы, а то сам знаешь, подарочек всё ещё у меня. И я тебя не проверил на нюх. Это была только разминка.
+        </Text>
+        <View style={styles.foxImg}>
+          <Image
+            source={fox3}
+            style={styles.fox3}
+          />
+        </View>
+        <Text style={styles.paragraph}>
+          А ну, скажи, сколько светил в твоём маленьком мирке?
         </Text>
 
         <Input
           onSubmitText={onClick}
         />
         <Button
-          text="Click on me"
+          text="Я посчитал все!"
           onPress={onClick}
         />
 
         <Hint
           count="1"
-          text="Number 1"
+          text="Лампочек сколько."
         />
         <Hint
           count="2"
-          text="Number 2"
+          text="Ну лампочек у тебя, таких чтобы БЛЫМ - и горят, или не горят, если не БЛЫМ. В общем, считай все, что на своих местах. И если включить, то могли бы светить =)"
+        />
+        <Hint
+          count="3"
+          text="Все - значит все, даже если надо заглянуть в самое тёмное место, особенно на кухне. Думай и ищи. Циферку вводи!"
         />
       </View>
     )
